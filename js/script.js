4c88839151a338fe5e6e3b851bb9a0c45a19aedb
michelecanini <studio.michelecanini@gmail.com>
2023-05-17 17:15:41 +0200

    [UPLOAD] insert preliminary data user

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 //CALCULATING THE PRICE OF A TRAIN TIKET
+
+let name_passenger = prompt ('Inserisci il nome del passeggero')
+console.log (name_passenger)
+let last_name_passenger = prompt ("Inserisci il cognome del passeggero")
+console.log (last_name_passenger)
+
+document.getElementById('hello_user').innerHTML += ' ' +name_passenger + ' ' + last_name_passenger;
+
+let km_travel = prompt ('Inserisci i km da percorrere')
+console.log (km_travel)
+let age_passenger = prompt ("Inserisci l'età del passeggero")
+console.log (age_passenger)
--- readme.md
+++ readme.md
@@ -1,2 +1,11 @@
-PROBLEM: CREATE A PROGRAM FOR CALCULATING THE PRICE OF A TRAIN TIKET
+PROBLEM: CREATE A PROGRAM FOR CALCULATING THE PRICE OF A TRAIN TIKET WITH SPECIFIC CHARACTERIST
 
+1. ask the user for the name passenger
+2. create a variable
+3. ask the user the lastname of the passenger
+4. create a variable
+5. ask the user for the number of kilometers to travel
+6. create a variable
+7. ask the user the age of the passenger
+8. create a variable
+9. effect debug in the console
