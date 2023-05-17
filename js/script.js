8492348ee9564ad5f052c12afc891d8800113f0b
michelecanini <studio.michelecanini@gmail.com>
2023-05-17 17:49:14 +0200

    [UPLOAD] adjustment code

--- js/script.js
+++ js/script.js
@@ -5,9 +5,12 @@ console.log (name_passenger)
 let last_name_passenger = prompt ("Inserisci il cognome del passeggero")
 console.log (last_name_passenger)
 
-document.getElementById('hello_user').innerHTML += ' ' +name_passenger + ' ' + last_name_passenger;
+document.getElementById('hello_user').innerHTML += ` ${name_passenger} ${last_name_passenger}`
 
 let km_travel = prompt ('Inserisci i km da percorrere')
 console.log (km_travel)
 let age_passenger = prompt ("Inserisci l'età del passeggero")
 console.log (age_passenger)
+
+document.getElementById('km_travel').innerHTML += ` ${km_travel}`
+document.getElementById('age_passenger').innerHTML += ` ${age_passenger}`
