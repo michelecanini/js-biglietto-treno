771e0efa7efee62152df0389eb1ec96df2a4e93b
michelecanini <studio.michelecanini@gmail.com>
2023-05-21 14:09:34 +0200

    [UPDATE] remove c-log end insert comment

--- js/script.js
+++ js/script.js
@@ -1,31 +1,26 @@
 //CALCULATING THE PRICE OF A TRAIN TIKET
 
+//INSERT NAME END LASTNAME OF PASSENGER
 let name_passenger = prompt ('Inserisci il nome del passeggero');
-console.log (name_passenger);
 let last_name_passenger = prompt ("Inserisci il cognome del passeggero");
-console.log (last_name_passenger);
 
 document.getElementById('hello_user').innerHTML += ` ${name_passenger} ${last_name_passenger}`;
 
+//INSERT KM END AGE PASSENGER
 let km_travel = prompt ('Inserisci i km da percorrere');
-console.log (km_travel);
 let age_passenger = prompt ("Inserisci l'età del passeggero");
-console.log (age_passenger)
 
 document.getElementById('km_travel').innerHTML += ` ${km_travel}`;
 document.getElementById('age_passenger').innerHTML += ` ${age_passenger}`;
 
+//INSERT VARIABLE PRICING
 let price_km = 0.21;
-console.log (price_km);
 let price_minors = 0.21 / 5 * 4;
-console.log (price_minors);
 let price_over = 0.21 / 5 * 3;
-console.log (price_over);
 
+//INSERT CONDITIONAL STATEMENT END CALCULATING THE PRICE
 if (age_passenger <= 17){
 document.getElementById('price_tiket').innerHTML += (km_travel * price_minors).toFixed(2);
-console.log (km_travel * price_minors) ;
-
 }else if ((age_passenger >= 18) && (age_passenger < 65)){ 
     document.getElementById('price_tiket').innerHTML += (km_travel * price_km).toFixed(2);
 }else if (age_passenger >= 65){
